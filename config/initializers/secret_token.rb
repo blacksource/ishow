3df57add189e33fe8c61dde85decf5f577102622
= <=>
2012-02-19 22:41:58 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ishow::Application.config.secret_token = '5bf5e5bfd33963626375ad3d26b5f4421ac2a5694e094642b24db80c757de34805669b8f31a89e24d8ce419b81c4edd2a76276b28ff9f38580877c794f0752b5'
